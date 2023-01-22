e93b1abe7766ffad2c618239c6f551e5ca560ffb
Dimitrie David <david.dimitrie@gmail.com>
2023-01-23 01:50:34 +0200

    added zsh

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 
+# install zsh
+sudo apt-get install zsh -y
+
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
