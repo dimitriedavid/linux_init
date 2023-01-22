ef18438a18f55b9b92c81084e514fcf939601001
Dimitrie David <david.dimitrie@gmail.com>
2023-01-23 01:59:15 +0200

    unattended install for oh-my-zsh

--- init.sh
+++ init.sh
@@ -5,4 +5,4 @@
 sudo apt-get install zsh -y
 
 # install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
