2c557d37ffc4baa21da9f6c9a8e75b4119e4b82f
Dimitrie David <david.dimitrie@gmail.com>
2023-01-23 00:03:58 +0000

    added fish-like-autosuggestions

--- init.sh
+++ init.sh
@@ -6,3 +6,8 @@ sudo apt-get install zsh -y
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+
+# install fish-like-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions)/g' ~/.zshrc
+source ~/.zshrc
